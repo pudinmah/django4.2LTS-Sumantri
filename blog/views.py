7b126deb5514df34549127f238db16ddde240993
Mahpudin <pudinmah69@gmail.com>
2024-02-05 04:06:07 +0700

    09 Manajemen URLS Project dan Aplikasi

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'blog.html')
+
+def cari(request):
+    return HttpResponse('<h1>Ini adalah halaman pencarian</h1>')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,16 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 from . import views #views project
-from blog import views as blogviews # views app
 from kontak import views as kontakviews
 
 urlpatterns = [
     path('', views.index),  # views project fun index
-    path('blog/', blogviews.index), # views app fun index
+    path('blog/', include('blog.urls')), # views app fun index
     path('kontak/', kontakviews.index),
+
 
 
     
