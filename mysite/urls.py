cddbe0abdb404d4dce5066b294c995a2cfff6b83
Mahpudin <pudinmah69@gmail.com>
2024-02-05 02:24:56 +0700

    08  Pengenalan, Installasi dan Setting Aplikasi

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.urls import path
 
 
-from . import views
+from . import views #views project
+from blog import views as blogviews # views app
+from kontak import views as kontakviews
 
 urlpatterns = [
-    path('', views.index),  # from . import views
-    path('about/', views.about),
+    path('', views.index),  # views project fun index
+    path('blog/', blogviews.index), # views app fun index
+    path('kontak/', kontakviews.index),
 
 
     
