1a0116e55b4aa2d1c6a5431116a67d12915ef6c7
Mahpudin <pudinmah69@gmail.com>
2024-02-08 01:56:27 +0700

    22 Menampilkan Data Models

--- blog/models.py
+++ blog/models.py
@@ -1,43 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Publication(models.Model):
-    name = models.CharField(max_length=50)
+class Kategori(models.Model):
+    kode = models.CharField(max_length=7)
+    nama = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return "%d. %s" % (self.id, self.nama)
+class Artikel(models.Model):
+    judul = models.CharField(max_length=100)
+    isi   = models.TextField()
 
-class Reader(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    pilih_kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
-
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    fill = models.TextField()
-    OPTION_CATEGORY = [
-        ('python','python'),
-        ('django','django'),
-        ('javascript','javascript'),
-    ]
-    categories = models.CharField(max_length=50, choices=OPTION_CATEGORY)
-    publications = models.ForeignKey(Publication, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag)
-
-    def __str__(self):
-        return self.title
-    
-class ReadList(models.Model):
-    readers = models.ForeignKey(Reader, on_delete=models.CASCADE)
-    articles = models.ForeignKey(Article, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.articles.title} - {self.readers.name}"
+        return "%d. %s" % (self.id, self.judul)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import Artikel
+
 # Create your views here.
+
+def index(request):
+    # queryset
+    allArtikel = Artikel.objects.all()
+    print(allArtikel)
+    
+    context = {
+        'title':'blog',
+        'headline':'halaman blog',
+        'semuaArtikel':allArtikel
+
+    }
+    return render(request, 'index.html', context)
